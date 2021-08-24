0870e2adb91e7afa3d609a932b732dd5fdb741ef
dilner1 <dan.milner871@gmail.com>
2021-08-24 14:56:24 +0000

    Adding DOM event listener

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const player = 1;
+    const winner = 0;
+    const playerColor = {};
+    playerColor[-1] = '#1D3557';
+    playerColor[1] = '#E63946';
+    const count = 0;
+
+    ${'chip'}.each(function) {
+        $(this)Attr("id", count);    }
+});

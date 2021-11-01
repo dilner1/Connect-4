5dd476d2e43ca9efe6be1bcc3e5bcbc997f6cec9
dilner1 <dan.milner871@gmail.com>
2021-11-01 16:55:02 +0000

    Working but playerTurnText not correct

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,10 @@
-const PLAYER_ONE_COLOR = 'rgb(230,57,70)'; 
+const PLAYER_ONE_COLOR = 'rgb(230,57,70)';
 const PLAYER_TWO_COLOR = 'rgb(29,53,87)';
-const EMPTY_SPACE_COLOR =  'snow';
+const EMPTY_SPACE_COLOR = 'snow';
 
 // link to player 1
-const USERNAME_1 =  document.getElementById('player-one-input');
-const USERNAME_2 =  document.getElementById('player-two-input');
+const USERNAME_1 = document.getElementById('player-one-input');
+const USERNAME_2 = document.getElementById('player-two-input');
 // link to player-one-display-text
 const P_1_DISPLAY_TEXT = document.getElementById('player-one-display-text');
 const P_2_DISPLAY_TEXT = document.getElementById('player-two-display-text');
@@ -13,6 +13,8 @@ const RESULT_OUTPUT1 = document.getElementById('page-one-output');
 const RESULT_OUTPUT2 = document.getElementById('page-two-output');
 // link to display-text-button
 const LOAD_BTTN = document.getElementById('loadbttn')
+const FORM = document.getElementById('form');
+
 //change to player1NameValue    
 var nameValue1 = [];
 var nameValue2 = [];
@@ -25,66 +27,50 @@ let tableData = document.getElementsByTagName('td');
 
 let resetButton = document.querySelector('.reset-game');
 
-const form = document.getElementById('form');
-
-/** load pages */
-window.addEventListener('DOMContentLoaded', pageLoad());
-
-function pageLoad(){
-    let page = document.body.id;
-        switch (page) {
-            case 'home-page':
-                console.log('On the Home Page');
-                homePageFormSubmit();
-                break;
-            case 'game-page':
-                console.log('On the Game Page');
-                let playerOneScore = parseInt(document.getElementById('player-1-score').innerHTML);
-                let playerTwoScore = parseInt(document.getElementById('player-2-score').innerHTML);
-                break;
-        }
-}
-
 /** Check if game has started and show which player turn it is */
 let playerTurnText = document.getElementById('player-go');
-    document.addEventListener('load', ()=>{
-        if (playerTurnText === null){
-            pass
-        } else{
-            playerTurnText.textContent = `${playerName} starts`;
-        }
-    })    
+document.addEventListener('load', () => {
+    if (playerTurnText === null) {
+        pass
+    } else {
+        playerTurnText.textContent = `${playerName} starts`;
+    }
+})
 let currentPlayerCount = 1;
 let playerChip = document.querySelectorAll('.chip');
 
-/** Add to player score on win */
-// function updatePlayerScores(){
-//     if (  playerOneScore && playerTwoScore === null ){
-//         pass
-//     } else {
-//         var playerOneScore = parseInt(document.getElementById('player-1-score').innerHTML);
-//         var playerTwoScore = parseInt(document.getElementById('player-2-score').innerHTML);
-//     }
-// }
-
-
-//document.addEventListener('load', updatePlayerScores);
-
-function homePageFormSubmit(){
-    function getUserNames(event){
+function homePageFormSubmit() {
+    function getUserNames(event) {
         event.preventDefault();
-        
+
         nameValue1 = USERNAME_1.value
         nameValue2 = USERNAME_2.value
         localStorage.setItem('playerOne', nameValue1);
         localStorage.setItem('playerTwo', nameValue2);
-    
+
         P_1_DISPLAY_TEXT.innerHTML = playerOne;
         P_2_DISPLAY_TEXT.innerHTML = playerTwo;
     };
-    form.addEventListener('submit', getUserNames);
+    FORM.addEventListener('submit', getUserNames);
 }
 
+/** load pages */
+window.addEventListener('DOMContentLoaded', loadPage());
+
+function loadPage() {
+    let page = document.body.id;
+    switch (page) {
+        case 'home-page':
+            console.log('On the Home Page');
+            homePageFormSubmit();
+            break;
+        case 'game-page':
+            console.log('On the Game Page');
+
+            //gamerScore();
+            break;
+    }
+}
 
 function resultsPage() {
 
@@ -95,7 +81,7 @@ document.addEventListener('click', resultsPage);
 
 /** changes player name */
 let playerName = '';
-document.addEventListener('click',changePlayerName)
+document.addEventListener('click', changePlayerName)
 
 function changePlayerName() {
     if (currentPlayerCount === 1) {
@@ -107,7 +93,7 @@ function changePlayerName() {
 
 /** check table cells for click and calls fuction to change color */
 Array.prototype.forEach.call(tableData, (event) => {
-    event.style.backgroundColor = EMPTY_SPACE_COLOR; 
+    event.style.backgroundColor = EMPTY_SPACE_COLOR;
     event.addEventListener('click', playerCellCheck);
 });
 
@@ -121,9 +107,8 @@ function playerCellCheck(event) {
             cell.push(tableRow[i].children[row]);
             if (currentPlayerCount === 1) {
                 cell[0].style.backgroundColor = PLAYER_ONE_COLOR;
-                // create a function that changes between currentPlayerCount? then executes only one of the following
 
-                /** check player 1 win condition    */
+                /** check player 1 win condition */
                 if (checkWinConditions()) {
                     return playerWinNotice();
                 } else if (checkCanvasSpace()) {
@@ -145,7 +130,7 @@ function playerCellCheck(event) {
                     return playerWinNotice();
 
                 } else if (checkCanvasSpace()) {
-                    
+
                     return playerDraw();
 
                 } else {
@@ -159,9 +144,16 @@ function playerCellCheck(event) {
     }
 };
 
+//put HERE 
+
+// function gamerScore(){
+     let playerOneScore = parseInt(document.getElementById('player-1-score').innerHTML);
+     let playerTwoScore = parseInt(document.getElementById('player-2-score').innerHTML);
+// };
+
 function checkWinConditions() {
     if (horizontalWinCheck() || verticalWinCheck() || diagonalWinCheckDown() || diagonalWinCheckUp()) {
-        if (currentPlayerCount === 1){
+        if (currentPlayerCount === 1) {
             playerTurnText.textContent = `${playerName} is the winner!`
             playerTurnText.style.color = PLAYER_ONE_COLOR
             return document.getElementById("player-1-score").innerText = ++playerOneScore;
@@ -171,8 +163,8 @@ function checkWinConditions() {
             return document.getElementById("player-2-score").innerText = ++playerTwoScore;
         }
     }
-}/** reset game on win */
-function playerWinNotice(){
+} /** reset game on win */
+function playerWinNotice() {
     swal.fire(`${playerName} wins!`);
     return setTimeout(resetGame, 2000)
 }
@@ -183,9 +175,7 @@ function playerDraw() {
     return setTimeout(resetGame, 2000)
 };
 
-/**
- * check if horizontal win condition is met
- */
+/** check if horizontal win condition is met */
 function horizontalWinCheck() {
     for (let row = 0; row < tableRow.length; row++) {
         for (let playerColor = 0; playerColor < 4; playerColor++) {
@@ -247,15 +237,13 @@ function diagonalWinCheckUp() {
 
 /** check if colors match */
 function checkChipsMatch(chip1, chip2, chip3, chip4) {
-    if (chip1 !== EMPTY_SPACE_COLOR){
+    if (chip1 !== EMPTY_SPACE_COLOR) {
         let c = chip1
         return (c === chip2 && c === chip3 && c === chip4)
-    } 
+    }
 };
 
-/**
- * Check if all slots have been taken by players - rewrite? 
- */
+/** Check if all slots have been taken by players - rewrite? */
 
 function checkCanvasSpace() {
     let canvasSlot = []
@@ -266,19 +254,20 @@ function checkCanvasSpace() {
     }
     if (canvasSlot.length === tableData.length) {
         return true;
-    }}
-    //let i = 0
-    /** not working yet - testing 
-    
-    
-    Array.prototype.forEach.call.tableData, (event) => {
-        event.style.backgroundColor = EMPTY_SPACE_COLOR;
-            console.log(tableData[i])
-        }
-    };*/
-    //const ages = [32, 33, 16, 40];
-    
-    /** tableData[i].every(checkCell)    // Returns false
+    }
+}
+//let i = 0
+/** not working yet - testing 
+
+
+Array.prototype.forEach.call.tableData, (event) => {
+    event.style.backgroundColor = EMPTY_SPACE_COLOR;
+        console.log(tableData[i])
+    }
+};*/
+//const ages = [32, 33, 16, 40];
+
+/** tableData[i].every(checkCell)    // Returns false
 
     function checkCell(cell) {
         cell.style.backgroundColor !== EMPTY_SPACE_COLOR;
@@ -289,9 +278,8 @@ function checkCanvasSpace() {
 /** Resets canvas */
 resetButton.addEventListener('click', resetGame);
 
-function resetGame () {
+function resetGame() {
     playerChip.forEach(chip => {
         chip.style.backgroundColor = EMPTY_SPACE_COLOR
     })
 };
-
